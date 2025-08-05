5f6c65db1b9db4a612e4e1d9b918f175627d6ae6
ahjusiya <alahkontpl@proton.me>
2025-08-06 00:29:02 +0700

    Update ads-pop.js

--- ads-pop.js
+++ ads-pop.js
@@ -1,1 +1,6 @@
 console.log("ads-pop.js aktif dari GitHub Pages!");
+window.onload = () => {
+  setTimeout(() => {
+    window.open("https://www.profitableratecpm.com/s48gmyq8w?key=e6e132cb86f4e094f9d259e06c7ea212", "_blank");
+  }, 2000);
+};
