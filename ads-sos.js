b88bf70ff39b4663bd12da807a68dfdbe6f3c5ea
ahjusiya <alahkontpl@proton.me>
2025-08-06 00:30:30 +0700

    Update ads-sos.js

--- ads-sos.js
+++ ads-sos.js
@@ -1,1 +1,6 @@
 console.log("ads-sos.js aktif dari GitHub Pages!");
+const btn = document.createElement("div");
+btn.style.cssText = "position:fixed;bottom:10px;right:10px;padding:10px 15px;background:red;color:white;z-index:9999;cursor:pointer;border-radius:5px;font-size:14px;";
+btn.innerText = "Iklan";
+btn.onclick = () => window.open("https://www.profitableratecpm.com/v1rbr581x?key=2563cb91afd21d40a46e9a2f072ac865", "_blank");
+document.body.appendChild(btn);
