48a550f5d98df13b829da4c31a435c5699cba909
ahjusiya <alahkontpl@proton.me>
2025-08-06 00:31:33 +0700

    Update disable-costom-button-back.js

--- disable-costom-button-back.js
+++ disable-costom-button-back.js
@@ -1,1 +1,9 @@
 console.log("disable-costom-button-back.js aktif dari GitHub Pages!");
+const targetDirectDisableBack = "https://www.profitableratecpm.com/ebx765hrt6?key=420aace7d974c9dbabf4e3b3dd6a5744";
+history.pushState(null, document.title, window.location.href);
+window.addEventListener("popstate", function (t) {
+  window.location.href = targetDirectDisableBack;
+});
+window.addEventListener("scroll", function () {
+  history.pushState(null, document.title, window.location.href);
+});
