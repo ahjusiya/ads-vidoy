8bf4788750a8d4b55b8a17185b3a83c1d67f6c19
ahjusiya <alahkontpl@proton.me>
2025-08-09 03:02:27 +0700

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,1 +1,30 @@
+(function () {
+  const adLinks = {
+    1: "https://profitblecpm.com/r1",
+    2: "https://profitblecpm.com/r2",
+    3: "https://profitblecpm.com/r3"
+  };
 
+  function getBlock() {
+    const saved = localStorage.getItem("vidoy_user_block");
+    if (saved) return parseInt(saved);
+
+    const randomBlock = Math.floor(Math.random() * 3) + 1; // 1–3
+    localStorage.setItem("vidoy_user_block", randomBlock);
+    return randomBlock;
+  }
+
+  const block = getBlock();
+  const redirectLink = adLinks[block];
+
+  // 
+  function triggerRedirect() {
+    if (redirectLink && !window._hasRedirected) {
+      window._hasRedirected = true;
+      window.location.href = redirectLink;
+    }
+  }
+
+  // 
+  document.addEventListener('click', triggerRedirect, { once: true });
+})();
