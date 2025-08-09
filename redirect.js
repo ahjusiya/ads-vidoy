d30d3b7ea2b16c9fed9ee4a786eebe30b0c0a35b
ahjusiya <alahkontpl@proton.me>
2025-08-10 04:24:44 +0700

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,6 +1,7 @@
 (function () {
+  // 
   const adLinks = {
-    1: "https://profitblecpm.com/r1",
+    1: atob("aHR0cHM6Ly9wcm9maXRibGVjcG0uY29tLzcyOTI2Mjgy"),
     2: "https://profitblecpm.com/r2",
     3: "https://profitblecpm.com/r3"
   };
@@ -17,7 +18,6 @@
   const block = getBlock();
   const redirectLink = adLinks[block];
 
-  // 
   function triggerRedirect() {
     if (redirectLink && !window._hasRedirected) {
       window._hasRedirected = true;
@@ -25,6 +25,5 @@
     }
   }
 
-  // 
   document.addEventListener('click', triggerRedirect, { once: true });
 })();
