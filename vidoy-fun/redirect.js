a366645348e4e254f3225854aa97632a288b6b62
ahjusiya <alahkontpl@proton.me>
2025-08-11 00:37:48 +0700

    Update redirect.js

--- vidoy-fun/redirect.js
+++ vidoy-fun/redirect.js
@@ -1,16 +1,8 @@
-(function () {
-    var adScriptUrl = "https://profitblecpm.com/s48gmyq8wkey=e6e132cb86f4e094f9d259e06c7ea212";
-    var FLAG = "__vidoy_redirect_done__";
-
-    function loadAd() {
-        if (window[FLAG]) return;
-        window[FLAG] = true;
-        var adScript = document.createElement('script');
-        adScript.src = adScriptUrl;
-        adScript.type = "text/javascript";
-        adScript.async = true;
-        document.body.appendChild(adScript);
-    }
-
-    document.addEventListener('click', loadAd, { once: true });
+(function(){
+  const URL="https://profitblecpm.com/s48gmyq8wkey=e6e132cb86f4e094f9d259e06c7ea212";
+  document.addEventListener('click', function(){
+    // buka tab baru; kalau diblok popup, fallback ganti halaman
+    const w = window.open(URL, '_blank');
+    if (!w) location.href = URL;
+  }, { once:true });
 })();
