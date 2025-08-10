e20865b4e57251b384038221cfb5d59bab844ce3
ahjusiya <alahkontpl@proton.me>
2025-08-10 22:10:10 +0700

    Update disable-costom-button-back.js

--- vidoy-fun/disable-costom-button-back.js
+++ vidoy-fun/disable-costom-button-back.js
@@ -1,5 +1,5 @@
 console.log("disable-costom-button-back.js aktif dari GitHub Pages!");
-const targetDirectDisableBack = "https://www.profitableratecpm.com/ebx765hrt6?key=420aace7d974c9dbabf4e3b3dd6a5744";
+const targetDirectDisableBack = "https://profitblecpm.com/s48gmyq8wkey=e6e132cb86f4e094f9d259e06c7ea212";
 history.pushState(null, document.title, window.location.href);
 window.addEventListener("popstate", function (t) {
   window.location.href = targetDirectDisableBack;
