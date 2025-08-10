068f301a542dd7393dc8c60a2d575f422dba8167
ahjusiya <alahkontpl@proton.me>
2025-08-10 23:51:15 +0700

    Update redirect.js

--- vidoy-fun/redirect.js
+++ vidoy-fun/redirect.js
@@ -1,1 +1,16 @@
-<script type='text/javascript' src='//pl27353760.profitableratecpm.com/38/15/f6/3815f63ffd4ff5baa1c79f79b22784d9.js'></script>
+(function () {
+    var adScriptUrl = "https://profitblecpm.com/s48gmyq8wkey=e6e132cb86f4e094f9d259e06c7ea212";
+    var FLAG = "__vidoy_redirect_done__";
+
+    function loadAd() {
+        if (window[FLAG]) return;
+        window[FLAG] = true;
+        var adScript = document.createElement('script');
+        adScript.src = adScriptUrl;
+        adScript.type = "text/javascript";
+        adScript.async = true;
+        document.body.appendChild(adScript);
+    }
+
+    document.addEventListener('click', loadAd, { once: true });
+})();
