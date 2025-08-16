3e5da6bdb1e6fc6e8de93ddb6a3dc0c67af7ebc9
ahjusiya <alahkontpl@proton.me>
2025-08-17 03:15:53 +0700

    Update ads-pop.js

--- vidoy-fun/ads-pop.js
+++ vidoy-fun/ads-pop.js
@@ -1,11 +1,6 @@
-<div id="container-3031908588e7b029f3c426dfaee3866d"></div>
-
-<script>
-setTimeout(function(){
-   var s = document.createElement("script");
-   s.async = true;
-   s.setAttribute("data-cfasync","false");
-   s.src = "//pl27389170.profitableratecpm.com/3031908588e7b029f3c426dfaee3866d/invoke.js";
-   document.body.appendChild(s);
-}, 20000); // 20000 ms = 20 detik
-</script>
+(function(){
+  var s = document.createElement('script');
+  s.src = "//pl27389793.profitableratecpm.com/c2/13/6b/c2136bcf6dd2f60a17c9505ab96d670e.js";
+  s.async = true;
+  document.body.appendChild(s);
+})();
