ff9a300cd6608531971937b2b62806bac7cde0b1
ahjusiya <alahkontpl@proton.me>
2025-08-31 18:28:18 +0700

    Update overlay.js

--- vidoy-fun/overlay.js
+++ vidoy-fun/overlay.js
@@ -3,7 +3,7 @@
     // === SETTING ===
     var target     = "https://vidoy.fun/cB6Tyq0.php"; 
     var cooldownMs = 10 * 1000;   
-    var delayMs    = 7000;        
+    var delayMs    = 3000;        
     // ===============
 
     if (!/^https:\/\//i.test(target)) return;
